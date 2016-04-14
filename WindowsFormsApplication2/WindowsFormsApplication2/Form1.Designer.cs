a20315c496a7667da768844ecd2dd892cc44e615
elessarsst <serhatturk@hotmail.com>
2016-04-14 15:18:13 +0300

    First Step
    My First Commit

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(413, 366);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "START";
+            this.buttonStart.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Inputs";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(250, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Selections";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Status";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 25);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(235, 210);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(253, 25);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox2.Size = new System.Drawing.Size(235, 210);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(12, 260);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox3.Size = new System.Drawing.Size(476, 100);
+            this.richTextBox3.TabIndex = 3;
+            this.richTextBox3.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 401);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+    }
+}
+
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        static IWebDriver driverGC;
+        static ChromeOptions options = new ChromeOptions();
+        System.Collections.ObjectModel.ReadOnlyCollection<OpenQA.Selenium.IWebElement> curInputElements, curAElements, curImgElements, curSpanElements, curRoleElements;
+        static UInt16 b12Profile = 1;
+        
+        
+        //System.Threading.Thread myThread = new System.Threading.Thread(myTest);
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+            driverGC.Quit();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            myTestStart();
+        }
+        public void myTestStart()
+        {
+            
+            driverGC = new ChromeDriver(@"C:\", options, new TimeSpan(0, 0, 0, 5));
+            driverGC.Navigate().GoToUrl("http://10.17.0.48/b12data");
+            driverGC.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(5));
+
+            leechElements();
+
+            
+            foreach (IWebElement elem in curInputElements)
+            {
+                string elemName;               
+                elemName = elem.GetAttribute("id");
+                richTextBox1.AppendText(elemName + "\n");
+            }
+            if(richTextBox1.Text == "")
+            {
+                updateStatus("Page Loading Error");
+            }
+            else if(richTextBox1.Find("kullanici") != -1)
+            {
+                updateStatus("Page Has Been Successfully Loaded");
+                updateStatus("Trying To Login");
+                myStep1();
+            }
+        }
+
+        public void myStep1()
+        {
+            foreach (IWebElement elem in curInputElements)
+            {
+                string elemName;
+
+                elemName = elem.GetAttribute("ID");
+                if(elemName == "kullanici")
+                {
+                    elem.SendKeys("MASTER");
+                }
+                else if(elemName == "Sifre")
+                {
+                    elem.SendKeys("12345A");
+                    elem.Submit();
+                    break;
+                }
+                
+            }
+
+            //System.Threading.Thread.Sleep(500);
+            //bool loadOkFlag = false;
+            //while (loadOkFlag == false)
+            //{
+            //    System.Collections.ObjectModel.ReadOnlyCollection<OpenQA.Selenium.IWebElement> tempElements;
+            //    tempElements = driverGC.FindElements(By.TagName("div"));
+            //    foreach (IWebElement elem in tempElements)
+            //    {
+            //        string elemName;
+            //        elemName = elem.GetAttribute("id");
+            //        if (elemName == "logo")
+            //        {
+            //            loadOkFlag = true;
+            //            break;
+            //        }
+            //    }
+            //}
+
+
+
+            leechElements();
+            richTextBox1.Text = "";
+            IWebElement baglanButton = null;
+            foreach (IWebElement elem in curInputElements)
+            {
+                string elemName;
+                elemName = elem.GetAttribute("id");
+                if(elemName == null || elemName == "")
+                {
+                    elemName = "hidden, value = " + elem.GetAttribute("value");
+                }
+                if(elemName == "baglan")
+                {
+                    baglanButton = elem;
+                }
+                richTextBox1.AppendText(elemName + "\n");
+            }
+            if (richTextBox1.Text == "")
+            {
+                updateStatus("Page Loading Error");
+            }
+            else if (richTextBox1.Find("kullanici") != -1)
+            {
+                updateStatus("Username or Password Doesn't Match! Stopped.");
+            }
+            else if(richTextBox1.Find("searchListView") != -1)
+            {
+                updateStatus("Login Successful");
+                if(richTextBox1.Find("value = 1") != -1 && richTextBox1.Find("value = 21") != -1 && richTextBox1.Find("value = 22") != -1)
+                {
+                    updateStatus("Profile List Is Up-To-Date");
+                    curRoleElements = driverGC.FindElements(By.CssSelector("[role=\"option\"]"));
+                    foreach (IWebElement elem in curRoleElements)
+                    {
+                        IWebElement subElement = null;
+                        subElement = elem.FindElement(By.TagName("input"));
+
+                        string elemName;
+                        elemName = subElement.GetAttribute("value");
+                        if (elemName == b12Profile.ToString())
+                        {
+                            elem.Click();
+                            baglanButton.Click();
+                            break;
+                        }
+                    }
+
+                }
+                else
+                {
+                    updateStatus("Profile List Is Out-Dated or Missing");
+                }
+            }
+
+        }
+
+        public void updateStatus(string statusString)
+        {
+            richTextBox3.AppendText(statusString + "\n");
+        }
+
+        public void leechElements()
+        {
+            curInputElements = driverGC.FindElements(By.TagName("input"));
+            curAElements = driverGC.FindElements(By.TagName("a"));
+            curImgElements = driverGC.FindElements(By.TagName("img"));
+            curSpanElements = driverGC.FindElements(By.TagName("span"));
+        }
+    }
+}
